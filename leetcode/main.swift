4ad2261287cf6004ec8a9a3d3e27b42f8697bcba
lingqineng <lingqineng@163.com>
2018-01-15 10:07:01 +0800

    Initial Commit

---
+++ leetcode/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  leetcode
+//
+//  Created by lingqineng on 2018/1/15.
+//  Copyright © 2018年 lingqineng. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
